e641f906f072058e72575a5352c12d79cee1212e
unknown <anujharad999@gmail.com>
2025-07-29 23:45:31 +0530

    added experience section

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaHtml5, FaCss3, FaReact, FaJs } from 'react-icons/fa';
+import { SiTailwindcss, SiMongodb, SiExpress, SiNodedotjs } from 'react-icons/si';
+
+const Experience = () => {
+  return (
+    <div>Experience</div>
+  )
+}
+
+export default Experience
