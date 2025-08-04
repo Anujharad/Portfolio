f89c132173d8be4540907470de4aa93c4cbfc09d
unknown <anujharad999@gmail.com>
2025-08-05 01:21:39 +0530

    updated

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import bannerImg from '../../assets/photo-C8q0KQHG.webp'
 const ProjectCard = ({title,description}) => {
   return (
-    <div className='bg-black bg-opacity-40 shadow-xl shadow-slate-900 w-2/5 pl-2 text-white '>
-        <img src={bannerImg} alt="" />
+    <div className='bg-black w-80 flex flex-col p-3 md:p-6 bg-opacity-40 shadow-xl rounded-2xl shadow-slate-900 p-3 text-white '>
+        <img className='p-4' src={bannerImg} alt="" />
         
-        <h1 className='p-2 font-bold text-2xl'>{title}</h1>
-        <div className='flex-wrap'>
-        <p className='text-sm '>{description}</p></div>
-        <div className='pt-5 items-center'>
-            <button className='bg-slate-700  p-2 flex wrap text-lg hover:bg-slate-400 rounded-3xl'>
-               Source Code 
+        <h1 className='px-4 font-bold text-xl  md:text-2xl leading-normal'>{title}</h1>
+        
+        <p className='text-sm  leading-tight px-4 md:text-md py-2'>{description}</p>
+        <div className=' items-center'>
+            <button className='bg-slate-700 md:p-4  p-2  flex wrap text-lg hover:bg-slate-400 rounded-3xl'>
+               Source Code
             </button>
         </div>
 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,9 +3,9 @@ import ProjectCard from './ProjectCard';
 
 const Projects = () => {
   return (<div>
-    <h1 className='text-white text-2xl md:text-4xl font-bold px-12 py-8'>Projects</h1>
-    <div>
-    <ProjectCard title={"web dev"} description={'NNSDKNDSKNSKNDSKNKNNOksdninsdnsdninin'} />
+    <h1 className='text-white flex flex-col text-2xl md:text-4xl font-bold px-12 py-8'>Projects</h1>
+    <div className=''>
+    <ProjectCard title={"web devcxnnnnovonfnvfofdf"} description={'NNSDKN8h8h8h8h8v h8h8h8h8h8DSKNSKNDSKNKNNOksdni nsdnsdninin'} />
      </div>
      
 
