1fad7f88273f3226e558b57eff4bbcb6da7e121f
unknown <anujharad999@gmail.com>
2025-08-26 01:32:26 +0530

    updated

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
   <div id="Projects" className='p-10 md:p-24 text-white'>
     <h1 className='text-white flex flex-col text-2xl md:text-4xl font-bold px-12 py-8'>Projects</h1>
     <div className='py-12 px-8 flex  flex-wrap  justify-around'>
-          <a href="hhttps://github.com/Anujharad/FlowPay---A-Digital-Wallet" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Anujharad/FlowPay---A-Digital-Wallet" target="_blank" rel="noopener noreferrer">
           <ProjectCard title={"FlowPay – A Digital Wallet"} description={'Designed and developed a secure full-stack digital wallet application using React.js, Tailwind CSS, Node.js, Express.js, and MongoDB.'} bannerImg={flowpay}/></a>
           <a href="https://github.com/Anujharad/Portfolio" target="_blank" rel="noopener noreferrer">
           <ProjectCard title={"Personal Portfolio Website"} description={'Built a responsive personal portfolio website to showcase projects and skills. Implemented using React.js and Tailwind CSS, focusing on clean design and smooth user experience.'} bannerImg={portfolioImg} /></a>
